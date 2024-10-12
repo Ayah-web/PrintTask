20d90704cf662b99bc1f56167339f72ab702b420
Ayahtullah Centeno <Ayahfahmy2007@gmail.com>
2024-10-12 18:46:20 +0400

    Create PrintStatementTask.py

---
+++ PrintStatementTask.py
@@ -0,0 +1,69 @@
+# print statement task by ayahtullah
+
+# basic string output
+print("Hello, world!")
+
+# simple math inside print
+print(1 + 1)
+print(1 * 2)
+print(1 / 2)
+
+# multiple arguements in print
+print("I", "like", "Code", "Lab", "one!")
+
+# print with escape characters
+print("She said,\"Good day!\"")
+
+# new-line character
+print("Hello sunshine\nGoodbye rain")
+
+# tab character
+print("Item1\tItem2\tItem3")
+
+# print multiple data types together
+print("You got a total of", 100)
+
+# print a single number
+print(18)
+
+# print a decimal number (float)
+print(18.01)
+
+# print special characters
+print("This is the shortened version of \"and\": &")
+
+# using sep perameter
+print("tiramisu", "coffee", "cream")
+
+# using end parameter
+print("Good,", end="")
+print("Evening")
+
+# print repeated string
+print("Hi!" * 3)
+
+# print a unicode character
+print("\u2764")
+
+# print an empty line
+print()
+
+# concatenating strings inside print
+print("snow" + "ball")
+
+# print a boolean
+print(True)
+print(False)
+
+# print exponential notation
+print(1.2e3)
+
+# print multiline text using triple quotes
+print("""This is a multiline 
+print statement.""")
+
+# print using escaped backlash
+print("This is a backlash:\\")
+
+
+
